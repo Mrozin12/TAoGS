eb8fa78e931e259610769cb72ac93febcfc9f6f8
Przemysław Mróz <przemek.mroz9@wp.pl>
2016-12-05 19:25:34 +0100

    Commit 2

--- Source.cpp
+++ Source.cpp
@@ -11,11 +11,17 @@ bool keys[ 5 ] = { false,false,false,false,false };
 //prototypy
 void InitShip(SpaceShip &ship);
 void DrawShip(SpaceShip &ship);
+void MoveShipUp(SpaceShip &ship);
+void MoveShipDown(SpaceShip &ship);
+void MoveShipLeft(SpaceShip &ship);
+void MoveShipRight(SpaceShip &ship);
 
 int main(void)
 {
 	//zmienna pierwotna
 	bool done = false;
+	bool redraw = true;
+	const int FPS = 60;
 
 	//zmienne obiektowe
 	SpaceShip ship;
@@ -23,6 +29,7 @@ int main(void)
 	//zmienne allegro
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
 
 	//inicjalizacja funkcji
 	if (!al_init())										
@@ -36,18 +43,95 @@ int main(void)
 	al_install_keyboard();
 
 	event_queue = al_create_event_queue();
+	timer = al_create_timer(1.0 / FPS);
 
 
 	InitShip(ship);
 
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	al_register_event_source(event_queue, al_get_display_event_source(display));
 
+	al_start_timer(timer);
 	while (!done)
 	{
-		DrawShip(ship);
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+		if (ev.type == ALLEGRO_EVENT_TIMER)
+		{
+			redraw = true;
+			if (keys[UP])
+				MoveShipUp(ship);
+			if (keys[DOWN])
+				MoveShipDown(ship);
+			if (keys[LEFT])
+				MoveShipLeft(ship);
+			if (keys[RIGHT])
+				MoveShipRight(ship);
+		}
+		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			done = true;
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+		{
+			switch (ev.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_ESCAPE:
+				done = true;
+				break;
+			case ALLEGRO_KEY_UP:
+				keys[UP] = true;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				keys[DOWN] = true;
+				break;
+			case ALLEGRO_KEY_LEFT:
+				keys[LEFT] = true;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+				keys[RIGHT] = true;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				keys[SPACE] = true;
+				break;
+			}
+		}
+		else if (ev.type == ALLEGRO_EVENT_KEY_UP)
+		{
+			switch (ev.keyboard.keycode)
+			{
+			case ALLEGRO_KEY_ESCAPE:
+				done = true;
+				break;
+			case ALLEGRO_KEY_UP:
+				keys[UP] = false;
+				break;
+			case ALLEGRO_KEY_DOWN:
+				keys[DOWN] = false;
+				break;
+			case ALLEGRO_KEY_LEFT:
+				keys[LEFT] = false;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+				keys[RIGHT] = false;
+				break;
+			case ALLEGRO_KEY_SPACE:
+				keys[SPACE] = false;
+				break;
+			}
+		}
 
-		al_flip_display();
-		al_clear_to_color(al_map_rgb(0, 0, 0));
+		
+		
+		if (redraw && al_event_queue_is_empty(event_queue))
+		{
+			redraw = false;
+			DrawShip(ship);
+
+			al_flip_display();
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+		}
 	}
 
 	al_destroy_display(display);						
@@ -74,4 +158,30 @@ void DrawShip(SpaceShip &ship)
 
 	al_draw_filled_triangle(ship.x - 12, ship.y - 17, ship.x + 12, ship.y, ship.x - 12, ship.y + 17, al_map_rgb(0, 255, 0));
 	al_draw_filled_rectangle(ship.x - 12, ship.y - 2, ship.x + 15, ship.y + 2, al_map_rgb(0, 0, 255));
+}
+void MoveShipUp(SpaceShip &ship)
+{
+	ship.y -= ship.speed;
+	if (ship.y < 0)
+		ship.y = 0;
+
+}
+
+void MoveShipDown(SpaceShip &ship)
+{
+	ship.y += ship.speed;
+	if (ship.y > HEIGHT)
+		ship.y = HEIGHT;
+}
+void MoveShipLeft(SpaceShip &ship)
+{
+	ship.x -= ship.speed;
+	if (ship.x < 0)
+		ship.x = 0;
+}
+void MoveShipRight(SpaceShip &ship)
+{
+	ship.x += ship.speed;
+	if (ship.x > 300)
+		ship.x = 300;
 }
